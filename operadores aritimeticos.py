c03af31be66607bfbaefca0ecd9bd7992dd3fd8a
MARCUS VINICIUS DA SILVA <202403025949@alunos.unifanor.edu.br>
2025-04-22 20:18:30 -0300

    novo assunto

---
+++ operadores aritimeticos.py
@@ -0,0 +1,10 @@
+
+# crie duas variaveis que recebe,respectivamente dois numeros.
+numero1 = 10
+numero2 = 15
+# imprima na tela:
+# soma
+# subtracao
+# multiplicacao
+# divisao
+# potencia
--- variavel.py
+++ variavel.py
@@ -1,2 +1,9 @@
-nome = "Hugo"
-print (nome);
+nome = "marcus"
+curso = "eng. civil" 
+anoCuuso = "3º semestre" 
+ira = 9.6
+print(nome,curso, anoCuuso, ira)
+print(f"teu nome é {nome}, você cursa{curso},está no {anoCuuso}e o teu ira é {ira}")
+# print (nome)
+
+
